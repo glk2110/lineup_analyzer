f2c9ade983361c8ec1d66480c93f9c83e41e1e9d
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-08-25 13:55:27 -0400

    done with gui

--- gui.py
+++ gui.py
@@ -2,18 +2,24 @@ from tkinter import *
 import os
 
 root = Tk()
-
-def OpenPro1():
+root.geometry("350x100")
+root.configure(background="wheat2")
+def runButton():
 	os.system('python3 analyzer.py '+ entry.get())
 	root.destroy()
 
+def runEnter(self):
+	os.system('python3 analyzer.py '+ entry.get())
+	root.destroy()
 
-
-label = Label(root, text = "Team Name")
-entry = Entry(root)
-button_1 = Button(root, text = "Run", command = OpenPro1)
-label.pack({"side":"left"})
-entry.pack({"side":"right"})
+frame = Frame(root)
+frame.pack({"side":"bottom"})
+label = Label(root, text = "Enter Team Name (ex. Columbia)", bg = 'wheat2')
+entry = Entry(root, highlightbackground='wheat2')
+button_1 = Button(root, text = "Run", command = runButton, width = 15, highlightbackground='wheat2')
+label.pack({"side":"top"})
 button_1.pack({"side":"bottom"})
+entry.pack({"side":"top"})
+root.bind("<Return>", runEnter)
 
 root.mainloop()
